17bb10c9f225b72b3eb8b2060618a7cb6d802039
EduardoPedrosa <eduardopedrosamf@gmail.com>
2023-04-24 11:42:02 -0300

    fix: add error treatment

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const axios = require('axios');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(morgan('combined'));
 app.use(express.json());
 
 app.all('*', async (req, res) => {
   try {
     const { method, url, body } = req;
     
+    console.log({
+        method,
+        url: url.replace('/', ''),
+        data: body,
+      })
     const response = await axios({
       method,
       url: url.replace('/', ''),
-      data: body,
+      data: Object.keys(body).length ? body : undefined,
     });
+
     res.status(response.status).json(response.data);
   } catch (error) {
-    console.log(error)
-    res.status(error.response.status).json(error.response.data);
+    res.status(error?.response?.status || 500).json(error?.response?.data || {});
   }
 });
 
