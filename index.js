ddf22a63b9c9b3abee04204208860021b3641c9d
EduardoPedrosa <eduardopedrosamf@gmail.com>
2023-04-24 11:33:15 -0300

    fix: remove headers

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ app.use(express.json());
 
 app.all('*', async (req, res) => {
   try {
-    const { method, url, headers, body } = req;
+    const { method, url, body } = req;
+    
     const response = await axios({
       method,
-      url: url.replace(req.baseUrl, ''),
-      headers,
+      url: url.replace('/', ''),
       data: body,
     });
     res.status(response.status).json(response.data);
   } catch (error) {
+    console.log(error)
     res.status(error.response.status).json(error.response.data);
   }
 });
